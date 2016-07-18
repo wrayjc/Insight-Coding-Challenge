22058bec8f461ed9b950486919c2f1d69e53a04c
wrayjc <wrayjc@vcu.edu>
2016-07-18 03:56:15 -0400

    Added comments to code

--- src/median_degree.java
+++ src/median_degree.java
@@ -14,70 +14,72 @@ import java.util.List;
 public class median_degree
 {
   public static void main(String[] args) throws FileNotFoundException              //declares a method called main
-  { 
-    String inputFileName = args[0];
-    String outputFileName = args[1];
+  {
+    String inputFileName = args[0];                                                //reads input filename from command line arguments
+    String outputFileName = args[1];                                               //reads output filename from command line arguments
 
-    //Construct the Scanner and PrintWriter objects for reading and writing
-    File inputFile = new File(inputFileName);
-    Scanner in = new Scanner(inputFile);
-    PrintWriter out = new PrintWriter(outputFileName);
+                                                             //Construct the Scanner and PrintWriter objects for reading and writing
+    File inputFile = new File(inputFileName);                //Construct File object to read-in file
+    Scanner in = new Scanner(inputFile);                     //Construct the Scanner object for reading
+    PrintWriter out = new PrintWriter(outputFileName);       //Construct the PrintWriter object for writing
 
-    ArrayList<String> created_time = new ArrayList<String>();
-    ArrayList<String> Actors = new ArrayList<String>();
-    ArrayList<String> Targets = new ArrayList<String>();
+    ArrayList<String> created_time = new ArrayList<String>(); //Construct ArrayList that stores the time_stamp for each transaction made
+    ArrayList<String> Actors = new ArrayList<String>();       //Construct ArrayList that stores the Actor Name for each transaction made
+    ArrayList<String> Targets = new ArrayList<String>();      //Construct ArrayList that stores the Target Name for each transaction made
+
                                                                          //Read the input and write the output
-    while (in.hasNext())                                                
+    while (in.hasNext())                                                 
     {                                                                    //Reads the String "created_time"
-       String timeField = in.next();                                   
-       String TimeField = timeField.substring(2,timeField.length() - 2);
-                                                                        //Reads in the TimeStamp
-       String timeStamp = in.next();
-       String TimeStamp = timeStamp.substring(1,timeStamp.length() - 2);
+       String timeField = in.next();                                     
+       String TimeField = timeField.substring(2,timeField.length() - 2); //Formats the String "created_time" down to created_time
+                                                                         //Reads in the TimeStamp
+       String timeStamp = in.next();                                     
+       String TimeStamp = timeStamp.substring(1,timeStamp.length() - 2); //Formats the time stamp string from "YYYY-MM-DDTHH:MM:SSz",  to the form YYYY-MM-DDTHH:MM:SSz
       
-       String targetField = in.next();                                  //Reads the String "target"
-       String TargetField = targetField.substring(1,targetField.length() - 2);
+       String targetField = in.next();                                   //Reads the String "target"
+       String TargetField = targetField.substring(1,targetField.length() - 2); //Formats "target" string to target
 
-       String target = in.next();
-       String Target = target.substring(1,target.length() - 2);        
-                                                                        //Reads the String "actor"
-       String actorField = in.next();
-       String ActorField = actorField.substring(1,actorField.length() - 2);
+       String target = in.next();                                        //Reads in the actual target per transaction
+       String Target = target.substring(1,target.length() - 2);          //Formats the target string by eliminating all characters (i.e. " and , at beginning and end) not associated with actual name
+                                                                         //Reads the String "actor"
+       String actorField = in.next();                                       
+       String ActorField = actorField.substring(1,actorField.length() - 2);  //Formats "actor" string to actor
 
-       String actor = in.next();                                       //Reads the Actor Name
-       String Actor = actor.substring(1,actor.length() - 2);
+       String actor = in.next();                                       //Reads in the Actor Name and stores it as a String
+       String Actor = actor.substring(1,actor.length() - 2);           //Formats the actor string by eliminating all characters (i.e. " and , at beginning and end of string) not associated with actor's actual name
        
-       created_time.add(timeStamp);
-       Actors.add(Actor);
-       Targets.add(Target);
+       created_time.add(timeStamp);                                    //Adds the timestamp for particular transaction to ArrayList
+       Actors.add(Actor);                                              //Adds the actor name for particular transaction to ArrayList
+       Targets.add(Target);                                            //Adds the target name for particular transaction to ArrayList
     }
-       created_time = TimeReader(created_time);                        
-       ArrayList<Double> Medians = OrderTransactions(created_time, Actors, Targets);
+       created_time = TimeReader(created_time);                        //Formats the time stamp string stored in ArrayList to a string of seconds
+       ArrayList<Double> Medians = OrderTransactions(created_time, Actors, Targets); //Calculates and Stores the rolling medians in an ArrayList per transaction made
        for(int i = 0; i < Medians.size(); i++)
-         out.printf("%.2f \n", Medians.get(i));
+         out.printf("%.2f \n", Medians.get(i));                         //Outputs the rolling medians calculated to output file
    
-    in.close();                                       //Closes Scanner File
-    out.close();                                      //Closes PrintWriter File
-  }
-  //Reads in a TimeStamp String and stores it in a usable form in this case a string of seconds
+    in.close();                                                         //Closes Scanner File
+    out.close();                                                        //Closes PrintWriter File
+  } 
+
+          //Reads in a TimeStamp String and stores it in a usable form in this case a string of seconds
   public static ArrayList<String> TimeReader(ArrayList<String> created_time) 
   {
     ArrayList<String> CalculatedTime = new ArrayList<String>();
-    int basis = 2016 - 3;
+    int basis = 2016 - 3;                                       //Creates a basis as reference for Processed Timestamp
     
     for(int i = 0; i < created_time.size(); i++)
     {
       String timeStamp = created_time.get(i);
-      int year = Integer.parseInt(created_time.get(i).substring(1,5).trim());
-      int month = Integer.parseInt(created_time.get(i).substring(6,8).trim());
-      int day = Integer.parseInt(created_time.get(i).substring(9,11).trim());
-      int hour = Integer.parseInt(created_time.get(i).substring(12,14).trim());
-      int minute = Integer.parseInt(created_time.get(i).substring(15,17).trim());
-      int second = Integer.parseInt(created_time.get(i).substring(18,20).trim());
+      int year = Integer.parseInt(created_time.get(i).substring(1,5).trim());       //Format time stamp extracting only the year and converting it to Integer stored value
+      int month = Integer.parseInt(created_time.get(i).substring(6,8).trim());      //Format time stamp extracting only the month and converting it to Integer stored value 
+      int day = Integer.parseInt(created_time.get(i).substring(9,11).trim());       //Format time stamp extracting only the day and converting it to Integer stored value
+      int hour = Integer.parseInt(created_time.get(i).substring(12,14).trim());     //Format time stamp extracting only the hour and converting it to Integer stored value
+      int minute = Integer.parseInt(created_time.get(i).substring(15,17).trim());   //Format time stamp extracting only the minute and converting it to Integer stored value
+      int second = Integer.parseInt(created_time.get(i).substring(18,20).trim());   //Format time stamp extracting only the second and converting it to Integer stored value
   
-      CalculatedTime.add(TimeProcessor(year, month, day, hour, minute, second, basis));
+      CalculatedTime.add(TimeProcessor(year, month, day, hour, minute, second, basis));  //Convert timestamp into total seconds from a zero reference point
     }    
-    return CalculatedTime;
+      return CalculatedTime;                                                          //Returns an ArrayList of Strings of numbers representing seconds from basis corresponding to particular timestamp processed
   }
 
   //Processes a TimeStamp into a mathematical quantity to be used in Analysis (i.e. total seconds from a defined basis) 
@@ -121,6 +123,7 @@ public class median_degree
     }
     return Medians;
   }
+ 
   //Eliminates Transactions outside of the 60 second window
   public static Double ScaledTransactions(ArrayList<String> created_time, ArrayList<String> Actors, ArrayList<String> Targets, int Transaction)
   {
@@ -140,6 +143,7 @@ public class median_degree
      }
    return Analyzer(Actors, Targets, Transaction);
   }
+ 
   //Generates Graph and Calculates Degrees to compute Rolling Median
   public static Double Analyzer(ArrayList<String> actors, ArrayList<String> targets, int Transaction)
   {
@@ -167,6 +171,7 @@ public class median_degree
     }
     return median(Degrees);
   }
+ 
   //Calculates the median per a transaction
   public static Double median(ArrayList<Integer> Degrees)
   {
